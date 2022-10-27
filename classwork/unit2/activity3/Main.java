a179d6e72de6f0dbb09e80d082467dd60744b662
dantefernando <62758801+dantefernando@users.noreply.github.com>
2022-10-27 10:53:09 -0400

    add compare method but it doesn't work

--- classwork/unit2/activity3/Main.java
+++ classwork/unit2/activity3/Main.java
@@ -1,9 +1,40 @@
 import java.util.Scanner;  // User stdin inputs 
+import java.util.Arrays;  // For Array sorting
+
 
 class Main {
 
+    static void compareWords(String word0, String word1) {
+
+        // Convert strings to char array
+        char[] word0Array = word0.toCharArray();
+        char[] word1Array = word1.toCharArray();
+
+        // Sort arrays alphabetically
+        Arrays.sort(word0Array);
+        Arrays.sort(word1Array);
+
+        // Convert arrays to strings
+        String word0Sorted = new String(word0Array);
+        String word1Sorted = new String(word1Array);
+
+        // Check if strings are equal
+        if (word0Sorted == word1Sorted) {
+            System.out.println("IS ANAGRAM!");
+            System.out.println(word0Sorted);
+            System.out.println(word1Sorted);
+        }
+        else {
+            System.out.println("NOT ANAGRAM!");
+            System.out.println(word0Sorted);
+            System.out.println(word1Sorted);
+        }
+
+    }
+
 
     // Get two words from user input
+    // Return array of both words
     static String[] getWords() {
         
         Scanner sc = new Scanner(System.in);
@@ -35,7 +66,7 @@ class Main {
         String word1 = words[1];
 
         // Check if words are anagrams
-        // compareWords(word0, word1);
+        compareWords(word0, word1);
 
     }
 
