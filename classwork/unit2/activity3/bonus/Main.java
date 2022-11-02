155ec2ffe1b63a4b922302a873f50ea5165be82b
dantefernando <62758801+dantefernando@users.noreply.github.com>
2022-11-01 22:04:07 -0400

    u2a3problem2 edited to match rubric pt1

--- classwork/unit2/activity3/bonus/Main.java
+++ classwork/unit2/activity3/bonus/Main.java
@@ -16,7 +16,7 @@ class Main {
 
     // Compare two words and check if they are anagrams
     // or not
-    static void compareWords(String word0, String word1) {
+    static boolean isAnagram(String word0, String word1) {
 
         // Convert strings to char arrays without spaces and all lowercase
         char[] word0Array = word0.replaceAll(" ", "").toLowerCase().toCharArray();
@@ -32,10 +32,10 @@ class Main {
 
         // Check if strings are equal
         if (word0Sorted.equals(word1Sorted)) {
-            System.out.println(word0 + " is an anagram of " + word1);
+            return true;
         }
         else {
-            System.out.println(word0 + " is NOT an anagram of " + word1);
+            return false;
         }
 
     }
@@ -64,17 +64,78 @@ class Main {
     }
 
 
+    // Interactive MENU acts as user interface 
+    // for user to choose operations
+    // and calculate values.
+    static void menu() {
+
+        // Loop until user wants to exit program
+        boolean menuLoop = true;
+        while (menuLoop == true) {
+
+            // Format Menu
+            String message = "\n===== Anagram Main Menu =====\n"+
+                             "1: Anagram Checker\n"+
+                             "===========================\n"+
+                             "0: Exit Program\n"+
+                             "===========================\n";
+
+            // Print Menu to stdout
+            System.out.println(message);
+
+            Scanner sc = new Scanner(System.in);
+
+            // Choice loop
+            boolean loop = true;
+
+            // Loop until user enters valid input
+            // if loop == false break loop
+            while (loop == true) {
+
+                System.out.print("Your choice: ");
+
+                // format input
+                String choice = sc.nextLine().toLowerCase();
+
+                switch (choice) {
+                    case "1":  // User wants to check if the words are anagrams
+
+                        // Get words from user stored in array
+                        String[] words = getWords();
+
+                        // Get individual words from array
+                        String word0 = words[0];
+                        String word1 = words[1];
+
+                        // Check if words are anagrams
+                        if (isAnagram(word0, word1) == true) {  // Words are anagrams
+                            System.out.println(word0 + " is an anagram of " + word1);
+                        }
+                        else {  // Words are not anagrams
+                            System.out.println(word0 + " is NOT an anagram of " + word1);
+                        }
+
+                        loop = false;
+                        break;
+
+                    case "0":  // User wants to exit program
+                        loop = false;
+                        menuLoop = false; // Stop menu loop and exit program
+                        break;
+                    default:  // User enters invalid input (no exceptions for erroneous user inputs)
+                        System.out.println("Please choose a valid option, try again...\n");
+                }
+            }
+        }
+    }
+
+
     // Main
     public static void main(String[] args) {
 
-        // Get words from user
-        String[] words = getWords();
+        // Menu
+        menu();
 
-        String word0 = words[0];
-        String word1 = words[1];
-
-        // Check if words are anagrams
-        compareWords(word0, word1);
-
+        System.out.println("Exited, Goodbye!"); // User has exited the main menu interface
     }
 }
--- classwork/unit2/activity3/problem2/Main.java
+++ classwork/unit2/activity3/problem2/Main.java
@@ -6,13 +6,15 @@ Description: A program that checks if two words are
              anagrams of each other WITHOUT USING SORTING METHODS.
 */
 
+
 import java.util.Scanner;  // User stdin inputs 
 
 class Main {
 
     // Method that compares two words and checks if they are
     // anagrams of each other WITHOUT using sorting methods
-    static void compareWords(String word0, String word1) {
+    // returns true if words are anagram and returns false if words aren't anagrams.
+    static boolean isAnagram(String word0, String word1) {
 
         // Convert strings to char arrays without spaces and all lowercase
         char[] word0Array = word0.replaceAll(" ", "").toLowerCase().toCharArray();
@@ -45,21 +47,16 @@ class Main {
 
         // Check if array is all 0's 
         // (both words are anagrams)
-        boolean isAnagram = true;
+        boolean anagram = true;
         for (int i : letterCount) {  // Iterate through elements of array
             if (i != 0) {  // Words are not anagrams of each other
-                isAnagram = false; 
+                anagram = false; 
                 break;  // break loop to prevent checking rest of letters
                         // unnecessarily
             }
         }
 
-        if (isAnagram == true) {  // Words are anagrams
-            System.out.println(word0 + " is an anagram of " + word1);
-        }
-        else {  // Words are not anagrams
-            System.out.println(word0 + " is NOT an anagram of " + word1);
-        }
+        return anagram;
     }
 
 
@@ -86,18 +83,79 @@ class Main {
     }
 
 
+    // Interactive MENU acts as user interface 
+    // for user to choose operations
+    // and calculate values.
+    static void menu() {
+
+        // Loop until user wants to exit program
+        boolean menuLoop = true;
+        while (menuLoop == true) {
+
+            // Format Menu
+            String message = "\n===== Anagram Main Menu =====\n"+
+                             "1: Anagram Checker\n"+
+                             "===========================\n"+
+                             "0: Exit Program\n"+
+                             "===========================\n";
+
+            // Print Menu to stdout
+            System.out.println(message);
+
+            Scanner sc = new Scanner(System.in);
+
+            // Choice loop
+            boolean loop = true;
+
+            // Loop until user enters valid input
+            // if loop == false break loop
+            while (loop == true) {
+
+                System.out.print("Your choice: ");
+
+                // format input
+                String choice = sc.nextLine().toLowerCase();
+
+                switch (choice) {
+                    case "1":  // User wants to check if the words are anagrams
+
+                        // Get words from user stored in array
+                        String[] words = getWords();
+
+                        // Get individual words from array
+                        String word0 = words[0];
+                        String word1 = words[1];
+
+                        // Check if words are anagrams
+                        if (isAnagram(word0, word1) == true) {  // Words are anagrams
+                            System.out.println(word0 + " is an anagram of " + word1);
+                        }
+                        else {  // Words are not anagrams
+                            System.out.println(word0 + " is NOT an anagram of " + word1);
+                        }
+
+                        loop = false;
+                        break;
+
+                    case "0":  // User wants to exit program
+                        loop = false;
+                        menuLoop = false; // Stop menu loop and exit program
+                        break;
+                    default:  // User enters invalid input (no exceptions for erroneous user inputs)
+                        System.out.println("Please choose a valid option, try again...\n");
+                }
+            }
+        }
+    }
+
+
     // Main
     public static void main(String[] args) {
 
-        // Get words from user stored in array
-        String[] words = getWords();
+        // Menu
+        menu();
 
-        // Get individual words from array
-        String word0 = words[0];
-        String word1 = words[1];
-
-        // Check if words are anagrams
-        compareWords(word0, word1);
-
+        System.out.println("Exited, Goodbye!"); // User has exited the main menu interface
     }
 }
+
