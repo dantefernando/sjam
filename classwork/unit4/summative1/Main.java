cf79f20982829c59a8bf86ff9651080b0bacaa2c
dantefernando <62758801+dantefernando@users.noreply.github.com>
2023-01-18 00:48:47 -0500

    started unit4 summative rectangle program

---
+++ classwork/unit4/summative1/Main.java
@@ -0,0 +1,31 @@
+/*
+File Name:  Main.java
+Programmer:  Dante F
+Date:  Wednesday, January 18, 2023
+Description: Creating a rectangle class
+*/
+
+import java.util.Scanner;  // For User stdin inputs and to read files
+import java.util.ArrayList;  // Import ArrayLists used for database
+import java.io.File; // import the File class
+import java.io.FileNotFoundException; // import class to handle errors
+
+
+class Main {
+
+
+    // perform a binary search on the file
+    // for the reference number
+    // static void binarySearch(String num, ArrayList<String> file) {
+    // }
+
+
+    // Main method
+    public static void main(String[] args) {
+
+        return;
+
+    }
+
+}
+
---
+++ classwork/unit4/summative1/Rectangle.java
@@ -0,0 +1,20 @@
+package summative1;
+
+public class Rectangle {
+
+    private int left;
+    private int bottom;
+    private int width;
+    private int height;
+
+
+    public Rectangle(int left, int bottom, int width, int height) {
+        this.left = left;
+        this.bottom = bottom;
+        this.width = width;
+        this.height = height;
+
+    }
+
+
+}
