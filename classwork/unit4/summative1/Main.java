4f76e323158efbaecbff7d70a181dd97398ea373
dantefernando <62758801+dantefernando@users.noreply.github.com>
2023-01-19 01:22:03 -0500

    u4a1: added more methods

--- classwork/unit4/summative1/Main.java
+++ classwork/unit4/summative1/Main.java
@@ -5,25 +5,28 @@ Date:  Wednesday, January 18, 2023
 Description: Creating a rectangle class
 */
 
-import java.util.Scanner;  // For User stdin inputs and to read files
-import java.util.ArrayList;  // Import ArrayLists used for database
-import java.io.File; // import the File class
-import java.io.FileNotFoundException; // import class to handle errors
+// import java.util.Scanner;  // For User stdin inputs and to read files
+// import java.util.ArrayList;  // Import ArrayLists used for database
+// import java.io.File; // import the File class
+// import java.io.FileNotFoundException; // import class to handle errors
 
 
 class Main {
 
 
-    // perform a binary search on the file
-    // for the reference number
-    // static void binarySearch(String num, ArrayList<String> file) {
-    // }
-
-
     // Main method
     public static void main(String[] args) {
 
-        return;
+        Rectangle rect = new Rectangle(-1,-2,21,9);
+
+        System.out.println(rect.left);
+        System.out.println(rect.bottom);
+        System.out.println(rect.width);
+        System.out.println(rect.height);
+
+        System.out.println(rect.toString());
+        System.out.println(rect.area());
+        System.out.println(rect.perimeter());
 
     }
 
--- classwork/unit4/summative1/Rectangle.java
+++ classwork/unit4/summative1/Rectangle.java
@@ -1,19 +1,59 @@
-package summative1;
 
 public class Rectangle {
 
-    private int left;
-    private int bottom;
-    private int width;
-    private int height;
+    int left, bottom, width, height;
+
+    // Method that returns the perimeter of the rectangle
+    public int perimeter() {
+        int perimeter = (this.width*2) + (this.height*2);
+        return perimeter;
+    }
 
 
+    // Method that returns the area of the rectangle
+    public int area() {
+        int area = this.width * this.height;
+        return area;
+    }
+
+
+    // Method that overrides toString default method
+    @Override
+    public String toString() {
+
+        // Format the output of the method
+        String output = "base: (" + this.left + "," + this.bottom + ") w:" + this.width + " h:" + this.height;
+
+        return output;
+
+    }
+
+
+    // Constructor with 4 parameters
     public Rectangle(int left, int bottom, int width, int height) {
         this.left = left;
         this.bottom = bottom;
         this.width = width;
         this.height = height;
 
+        // width is less than 0
+        if (this.width < 0) {
+            this.width = 0;  // Change to 0
+        }
+
+        // height is less than 0
+        if (this.height < 0) {
+            this.height = 0;  // Change to 0
+        }
+    }
+
+
+    // Constructor with no parameters
+    public Rectangle() {
+        this.left = 0;
+        this.bottom = 0;
+        this.width = 0;
+        this.height = 0;
     }
 
 
